d85192330c892e481c1a57b537d66b4c5bb96299
Katsuma Narisawa <face.the.music721@gmail.com>
2017-09-30 20:15:12 +0900

    Initialized by 'bundle gem'

---
+++ lib/simple_sitemap_generator.rb
@@ -0,0 +1,5 @@
+require "simple_sitemap_generator/version"
+
+module SimpleSitemapGenerator
+  # Your code goes here...
+end
---
+++ spec/simple_sitemap_generator_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe SimpleSitemapGenerator do
+  it "has a version number" do
+    expect(SimpleSitemapGenerator::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
