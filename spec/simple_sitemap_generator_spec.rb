6e18d7efafa84bfd82ccf00d9c9d97a13587e2b5
Katsuma Narisawa <face.the.music721@gmail.com>
2017-09-30 20:25:25 +0900

    Add specs

--- spec/simple_sitemap_generator_spec.rb
+++ spec/simple_sitemap_generator_spec.rb
@@ -1,11 +1,21 @@
-require "spec_helper"
+require 'spec_helper'
 
 RSpec.describe SimpleSitemapGenerator do
-  it "has a version number" do
+  it 'has a version number' do
     expect(SimpleSitemapGenerator::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'works' do
+    xml = <<~EOT
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset hoge:hoge>
+  <url>
+    <loc>/</loc>
+    <changefreq>daily</changefreq>
+    <priority>0.5</priority>
+  </url>
+</urlset>
+EOT
+    expect(SimpleSitemapGenerator::Sitemap.generate_xml).to eq(xml)
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,57 @@
-require "bundler/setup"
-require "simple_sitemap_generator"
+require 'simple_sitemap_generator'
+require 'active_support'
+require 'active_support/core_ext'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  class PathMock
+    attr_reader :path
+
+    def initialize(path)
+      @path = path
+    end
+
+    def spec
+      path
+    end
+  end
+
+  class RouteMock
+    attr_reader :verb, :path
+
+    def initialize(verb, path)
+      @verb = verb
+      @path = PathMock.new(path)
+    end
+  end
+
+  class RouteSetMock
+    def routes
+      [{
+        verb: 'GET',
+        path: '/'
+      }].map { |x| RouteMock.new(x[:verb], x[:path]) }
+    end
+  end
+
+  class RailsApplicationMock
+    def routes
+      RouteSetMock.new
+    end
+  end
+
+  module Rails
+    def self.application
+      RailsApplicationMock.new
+    end
   end
 end
